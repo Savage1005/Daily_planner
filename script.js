5ef728d2d53329a61f9de38202e47c5d58cf0096
Tom Savage <tsavage@gmail.com>
2020-05-09 18:03:52 -0400

    first push

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+
+var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+var currentHour = new Date().getHours();//Current hour in military time
+
+
